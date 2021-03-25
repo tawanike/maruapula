997aa7305b8b8c50d10920e1fc2a6474760cf281
Tatenda <tjmakunike@gmail.com>
2021-03-25 18:09:08 +0200

    changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,38 +1,40 @@
-import { jsx } from 'theme-ui'
-import { ThemeProvider } from 'theme-ui';
-import theme from 'src/theme';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'antd/dist/antd.css';
-import 'src/styles/globals.scss'
-import { Layout } from 'antd';
-import Header from 'src/components/Header';
-import Footer from 'src/components/Footer';
-import Sidebar from 'src/components/Sidebar';
-import CartDrawer from 'src/components/Cart/Drawer';
-import CartContextProvider from 'src/components/Cart/context';
+import { jsx } from "theme-ui"
+import { ThemeProvider } from "theme-ui"
+import theme from "src/theme"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "antd/dist/antd.css"
+import "src/styles/globals.scss"
+import { Layout } from "antd"
+import Header from "src/components/Header"
+import Footer from "src/components/Footer"
+import Sidebar from "src/components/Sidebar"
+import CartDrawer from "src/components/Cart/Drawer"
+import CartContextProvider from "src/components/Cart/context"
 
-const { Sider, Content } = Layout;
+const { Sider, Content } = Layout
 
 function App({ Component, pageProps }) {
-  return <CartContextProvider>
-    <ThemeProvider theme={theme}>
-      <Layout>
-        <Header />
+  return (
+    <CartContextProvider>
+      <ThemeProvider theme={theme}>
         <Layout>
-          <Sider width={260}>
-            <Sidebar />
-          </Sider>
-          
-          <Content className="pt-3 p-5">
-            <Component {...pageProps} />
-            <CartDrawer />
-          </Content>
-        </Layout>
+          <Header />
+          <Layout>
+            <Sider width={260}>
+              <Sidebar />
+            </Sider>
 
-        <Footer />
-      </Layout>
+            <Content className="pt-3 p-5">
+              <Component {...pageProps} />
+              <CartDrawer />
+            </Content>
+          </Layout>
+
+          <Footer />
+        </Layout>
       </ThemeProvider>
     </CartContextProvider>
+  )
 }
 
-export default App;
+export default App
