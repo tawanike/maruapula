f4f9a8223f5f812569869f687d12e0773aeb1132
Tatenda <tjmakunike@gmail.com>
2021-03-26 16:49:11 +0200

    put space between list items

--- src/_theme/colors.tsx
+++ src/_theme/colors.tsx
@@ -6,20 +6,7 @@ export default {
   secondary: "#151522",
   accent: "#f05365",
   highlight: "",
-  muted: "#7dd181",
+  muted: "#79bf45",
   orange: "#FA9E57",
-  borderColor: "#b3b3cf",
-  modes: {
-    dark: {
-      text: "#c7c7c7",
-      textAlt: "#b3b3cf",
-      background: "#151522",
-      primary: "#292941",
-      secondary: "#e2e2e2",
-      accent: "#f05365",
-      highlight: "",
-      muted: "#7dd181",
-      borderColor: "#3d3d60"
-    }
-  }
+  borderColor: "#b3b3cf"
 }
