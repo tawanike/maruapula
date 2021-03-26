3482eb39f9f1424d71df6ec61f1462b87a536e0b
Tatenda <tjmakunike@gmail.com>
2021-03-26 21:39:40 +0200

    reconcile template

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,100 +1,138 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { jsx } from "theme-ui"
+import { jsx } from "theme-ui";
 
-import { Menu } from "antd"
-import { useRouter } from "next/router"
-import { useContext } from "react"
-import { ProductContext } from "src/components/Products/context"
-import { filterProducts, selectedCategory } from "src/components/Products/actions"
+import { Menu } from "antd";
+import { useRouter } from "next/router";
+import { useContext } from "react";
+import { ProductContext } from "src/components/Products/context";
+import {
+    filterProducts,
+    selectedCategory,
+} from "src/components/Products/actions";
 
 export default function Sidebar() {
-  const router = useRouter();
-  const productContext = useContext(ProductContext);
-  const navigate = e => {
-    if (e.key === 'Specials') {
-      const defaultCategoryProducts: any[] = productContext.state.products.filter(product => {
-        if(product.specials === 'Yes') {
-          console.log('CATEGORY', product)
-          return product;
+    const router = useRouter();
+    const productContext = useContext(ProductContext);
+    const navigate = (e) => {
+        if (e.key === "Specials") {
+            const defaultCategoryProducts: any[] = productContext.state.products.filter(
+                (product) => {
+                    if (product.specials === "Yes") {
+                        console.log("CATEGORY", product);
+                        return product;
+                    }
+                }
+            );
+            productContext.dispatch(selectedCategory(e.key));
+            productContext.dispatch(filterProducts(defaultCategoryProducts));
+        } else if (e.key === "Catering") {
+            router.push("/business");
+        } else if (e.key === "Businesses") {
+            router.push("/business");
+        } else {
+            const defaultCategoryProducts: any[] = productContext.state.products.filter(
+                (product) => {
+                    if (product.category === e.key) {
+                        return product;
+                    }
+                }
+            );
+            productContext.dispatch(selectedCategory(e.key));
+            productContext.dispatch(filterProducts(defaultCategoryProducts));
         }
-      });
-      productContext.dispatch(selectedCategory(e.key));
-      productContext.dispatch(filterProducts(defaultCategoryProducts));
-    } else if (e.key === 'Catering') {
-      router.push('/business')
-    } else if (e.key === 'Businesses') {
-      router.push('/business')
-    }else {
-      const defaultCategoryProducts: any[] = productContext.state.products.filter(product => {
-        if(product.category === e.key) {
-          return product;
-        }
-      });
-      productContext.dispatch(selectedCategory(e.key));
-      productContext.dispatch(filterProducts(defaultCategoryProducts));
-    }
+    };
 
-
-  };
-
-
-
-  return (
-    <Menu mode="inline" style={{ width: "100%" }} onClick={navigate} defaultSelectedKeys={[productContext.state ? productContext.state.category : ""]}>
-      <Menu.Item
-      key="Specials" className="text-warning"
-        sx={{
-          fontSize: "16px",
-          fontWeight: "bold",
-          "&:hover": { color: "muted", bg: "#eaffeb" }
-        }}
-      >
-        Specials
-      </Menu.Item>
-      <Menu.Item
-      key="Fruits"
-        sx={{
-          fontSize: "16px",
-          fontWeight: "bold",
-          "&:hover": { color: "muted", bg: "#eaffeb" }
-        }}
-      >
-        Fruits
-      </Menu.Item>
-      <Menu.Item
-       key="Vegetables"
-        sx={{
-          fontSize: "16px",
-          fontWeight: "bold",
-          "&:hover": { color: "muted", bg: "#eaffeb" }
-        }}
-      >
-        Vegetables
-      </Menu.Item>
-      <Menu.Item
-      key="Poultry"
-        sx={{
-          fontSize: "16px",
-          fontWeight: "bold",
-          "&:hover": { color: "muted", bg: "#eaffeb" }
-        }}
-      >
-        Poultry
-      </Menu.Item>
-      <Menu.Item
-      key="Smoothies"
-        sx={{
-          fontSize: "16px",
-          fontWeight: "bold",
-          "&:hover": { color: "muted", bg: "#eaffeb" }
-        }}
-      >
-        Smoothies
-      </Menu.Item>
-      <Menu.Item key="Precooked">Pre-cooked meals</Menu.Item>
-      <Menu.Item key="Catering">Catering</Menu.Item>
-      <Menu.Item key="Businesses">For Businesses</Menu.Item>
-    </Menu>
-  )
+    return (
+        <Menu
+            mode="inline"
+            style={{ width: "100%" }}
+            onClick={navigate}
+            defaultSelectedKeys={[
+                productContext.state ? productContext.state.category : "",
+            ]}
+        >
+            <Menu.Item
+                key="Specials"
+                className="text-warning"
+                sx={{
+                    fontSize: "16px",
+                    fontWeight: "bold",
+                    "&:hover": { color: "muted", bg: "#eaffeb" },
+                }}
+            >
+                Specials
+            </Menu.Item>
+            <Menu.Item
+                key="Fruits"
+                sx={{
+                    fontSize: "16px",
+                    fontWeight: "bold",
+                    "&:hover": { color: "muted", bg: "#eaffeb" },
+                }}
+            >
+                Fruits
+            </Menu.Item>
+            <Menu.Item
+                key="Vegetables"
+                sx={{
+                    fontSize: "16px",
+                    fontWeight: "bold",
+                    "&:hover": { color: "muted", bg: "#eaffeb" },
+                }}
+            >
+                Vegetables
+            </Menu.Item>
+            <Menu.Item
+                key="Poultry"
+                sx={{
+                    fontSize: "16px",
+                    fontWeight: "bold",
+                    "&:hover": { color: "muted", bg: "#eaffeb" },
+                }}
+            >
+                Poultry
+            </Menu.Item>
+            <Menu.Item
+                key="Smoothies"
+                sx={{
+                    fontSize: "16px",
+                    fontWeight: "bold",
+                    "&:hover": { color: "muted", bg: "#eaffeb" },
+                }}
+            >
+                Smoothies
+            </Menu.Item>
+            <Menu.Item
+                sx={{
+                    fontSize: "16px",
+                    fontWeight: "bold",
+                    "&:hover": { color: "muted", bg: "#eaffeb" },
+                }}
+                key="Precooked"
+            >
+                Pre-cooked meals
+            </Menu.Item>
+            <Menu.Item
+                sx={{
+                    fontSize: "16px",
+                    fontWeight: "bold",
+                    "&:hover": { color: "muted", bg: "#eaffeb" },
+                }}
+                key="Catering"
+            >
+                Catering
+            </Menu.Item>
+            <Menu.Item
+                sx={{
+                    fontSize: "16px",
+                    fontWeight: "bold",
+                    "&:hover": { color: "muted", bg: "#eaffeb" },
+                }}
+                key="Businesses"
+            >
+                For Businesses
+            </Menu.Item>
+        </Menu>
+    );
 }
