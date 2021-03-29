f3604d0a27764b6c6c2e8682edd3ba3d91a3b616
Tatenda <tjmakunike@gmail.com>
2021-03-29 20:12:54 +0200

    fixed font and side bar

--- src/components/Cart/Drawer/index.tsx
+++ src/components/Cart/Drawer/index.tsx
@@ -1,83 +1,126 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { jsx } from "theme-ui"
-import Image from "next/image"
-import { useRouter } from "next/router"
+import { jsx } from "theme-ui";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
-import { useContext } from "react"
-import { CartContext } from "src/components/Cart/context"
-import { Button, Drawer } from "antd"
-import { toggleCartDrawer } from "src/components/Cart/actions"
-import { removeFromCart } from 'src/components/Cart/actions';
+import { useContext } from "react";
+import { CartContext } from "src/components/Cart/context";
+import { Button, Drawer } from "antd";
+import { toggleCartDrawer } from "src/components/Cart/actions";
+import { removeFromCart } from "src/components/Cart/actions";
+import { CloseCircleFilled } from "@ant-design/icons";
 
 export default function CartDrawer(props) {
-  const router = useRouter()
-  const cartContext = useContext(CartContext)
+    const router = useRouter();
+    const cartContext = useContext(CartContext);
 
-  const onClose = () => {
-    cartContext.dispatch(toggleCartDrawer(!cartContext.state.drawer.visible))
-  }
+    const onClose = () => {
+        cartContext.dispatch(
+            toggleCartDrawer(!cartContext.state.drawer.visible)
+        );
+    };
 
-  const removeProductFromCart = (product: any) => {
-    cartContext.dispatch(removeFromCart(product.id));
-  }
+    const removeProductFromCart = (product: any) => {
+        cartContext.dispatch(removeFromCart(product.id));
+    };
 
-  return (
-    <Drawer
-      sx={{ zIndex: "9999" }}
-      title="My Shopping Cart"
-      placement="right"
-      closable={true}
-      onClose={onClose}
-      visible={cartContext.state.drawer.visible}
-      width={360}
-    >
-      {cartContext.state.products &&
-        cartContext.state.products.map(product => {
-          return (
-            <div key={product.id} className="CartItem row mb-4 pb-2" sx={{ borderBottom: "1px solid #e2e2e2" }}>
-              <div className="col-md-4">
-                <Image
-                  src={`https://res.cloudinary.com/mmogomedia/image/upload/v1616594498/maruapula/products/${product.id}.jpg`}
-                  alt={product.title}
-                  width={240}
-                  height={240}
-                />
-              </div>
-              <div className="col-md-6">
-                <p className="mb-2" sx={{ fontSize: "16px", fontWeight: "600" }}>
-                  {product.title}
-                </p>
-                <p className="mb-2" sx={{ fontSize: "16px", fontWeight: "500", color: "muted" }}>
-                  R{product.price}
-                </p>
-                <p className="mb-2" sx={{}}>
-                  Quantity: {product.quantity}
-                </p>
-              </div>
-              <div className="col-md-1">
-                <Button onClick={() => removeProductFromCart(product)}>X</Button>
-              </div>
-            </div>
-          )
-        })}
-        <div>
-          <p>Subtotal: R{cartContext.state.subtotal}</p>
-          <p>Delivery Fee: R50.00</p>
-          <p>Total: R{cartContext.state.subtotal + cartContext.state.serviceFee}</p>
-        </div>
-        <div className="row">
-          <div
-          sx={{ variant: "containers.button", bg: "orange" }}
-          className="col-12 py-2"
-          onClick={() => {
-            router.push("/cart/checkout")
-            onClose()
-          }}
+    return (
+        <Drawer
+            sx={{ zIndex: "9999" }}
+            title="My Shopping Cart"
+            placement="right"
+            closable={true}
+            onClose={onClose}
+            visible={cartContext.state.drawer.visible}
+            width={360}
         >
-          <p>Checkout</p>
-        </div>
-      </div>
-    </Drawer>
-  )
+            {cartContext.state.products &&
+                cartContext.state.products.map((product) => {
+                    return (
+                        <div
+                            key={product.id}
+                            className="CartItem row mb-4 pb-2"
+                            sx={{ borderBottom: "1px solid #e2e2e2" }}
+                        >
+                            <div
+                                className="col-md-4"
+                                sx={{ display: "grid", placeItems: "center" }}
+                            >
+                                <img
+                                    src={`https://res.cloudinary.com/mmogomedia/image/upload/v1616594498/maruapula/products/${product.id}.jpg`}
+                                    alt={product.title}
+                                    onError={(e) => {
+                                        (e.target as any).src =
+                                            "./placeholder.png";
+                                        (e.target as any).style.width = "40%";
+                                        (e.target as any).style.height = "auto";
+                                        (e.target as any).style.marginTop =
+                                            "15px";
+                                        (e.target as any).style.marginBottom =
+                                            "15px";
+                                    }}
+                                    sx={{ width: "100%", height: "auto" }}
+                                />
+                            </div>
+                            <div className="col-md-6">
+                                <p
+                                    className="mb-2"
+                                    sx={{ fontSize: "16px", fontWeight: "600" }}
+                                >
+                                    {product.title}
+                                </p>
+                                <p
+                                    className="mb-2"
+                                    sx={{
+                                        fontSize: "16px",
+                                        fontWeight: "500",
+                                        color: "muted",
+                                    }}
+                                >
+                                    R{product.price}
+                                </p>
+                                <p className="mb-2" sx={{}}>
+                                    Quantity: {product.quantity}
+                                </p>
+                            </div>
+                            <div className="col-md-1">
+                                <CloseCircleFilled
+                                    onClick={() =>
+                                        removeProductFromCart(product)
+                                    }
+                                    sx={{
+                                        fontSize: "22px",
+                                        color: "orange",
+                                        "&:hover": {
+                                            color: "muted",
+                                        },
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    );
+                })}
+            <div>
+                <p>Subtotal: R{cartContext.state.subtotal}</p>
+                <p>Delivery Fee: R50.00</p>
+                <p>
+                    Total: R
+                    {cartContext.state.subtotal + cartContext.state.serviceFee}
+                </p>
+            </div>
+            <div className="row">
+                <div
+                    sx={{ variant: "containers.button", bg: "orange" }}
+                    className="col-12 py-2"
+                    onClick={() => {
+                        router.push("/cart/checkout");
+                        onClose();
+                    }}
+                >
+                    <p>Checkout</p>
+                </div>
+            </div>
+        </Drawer>
+    );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,85 +1,109 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { jsx } from "theme-ui"
-import { ThemeProvider } from "theme-ui"
-import theme from "src/_theme"
-import "bootstrap/dist/css/bootstrap.min.css"
-import "antd/dist/antd.css"
-import "../../style.scss"
-import NProgress from "nprogress"
-import "nprogress/nprogress.css"
-import { Layout } from "antd"
-import Header from "src/components/Header"
-import Footer from "src/components/Footer"
-import CartDrawer from "src/components/Cart/Drawer"
-import CartContextProvider from "src/components/Cart/context"
-import BannerContextProvider from "src/components/Banners/context"
-import ProductContextProvider from "src/components/Products/context"
-import { useEffect } from "react"
-import { Router } from "next/router"
-import '../libs/fontawesome'
+import { jsx } from "theme-ui";
+import { ThemeProvider } from "theme-ui";
+import theme from "src/_theme";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "antd/dist/antd.css";
+import "../../style.scss";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import { Layout } from "antd";
+import Header from "src/components/Header";
+import Footer from "src/components/Footer";
+import CartDrawer from "src/components/Cart/Drawer";
+import CartContextProvider from "src/components/Cart/context";
+import BannerContextProvider from "src/components/Banners/context";
+import ProductContextProvider from "src/components/Products/context";
+import { useEffect } from "react";
+import { Router } from "next/router";
+import "../libs/fontawesome";
 
 NProgress.configure({
-  showSpinner: false,
-  easing: "ease",
-  speed: 300,
-  trickleRate: 0.1,
-  trickleSpeed: 600
-})
+    showSpinner: false,
+    easing: "ease",
+    speed: 300,
+    trickleRate: 0.1,
+    trickleSpeed: 600,
+});
 
-const { Sider, Content } = Layout
+const { Sider, Content } = Layout;
 
 function App({ Component, pageProps }) {
-  useEffect(() => {
-    const handleRouteChange = url => NProgress.start()
-    const handleCompleteChange = url => NProgress.done()
-    const handleChangeError = (err, url) => NProgress.done()
+    useEffect(() => {
+        const handleRouteChange = (url) => NProgress.start();
+        const handleCompleteChange = (url) => NProgress.done();
+        const handleChangeError = (err, url) => NProgress.done();
 
-    Router.events.on("routeChangeStart", handleRouteChange)
-    Router.events.on("routeChangeComplete", handleCompleteChange)
-    Router.events.on("routeChangeError", handleChangeError)
+        Router.events.on("routeChangeStart", handleRouteChange);
+        Router.events.on("routeChangeComplete", handleCompleteChange);
+        Router.events.on("routeChangeError", handleChangeError);
 
-    return () => {
-      Router.events.off("routeChangeStart", handleRouteChange)
-      Router.events.off("routeChangeComplete", handleCompleteChange)
-      Router.events.off("routeChangeError", handleChangeError)
-    }
-  }, [])
-  return (
-    <BannerContextProvider>
-      <ProductContextProvider>
-    <CartContextProvider>
-      <ThemeProvider theme={theme}>
-        <div className="col-12 pt-2" sx={{ bg: "muted", height: "40px", textAlign: "center", color: '#FFFFFF'}}>
-              <div className="container">
-                All orders received before 14:00, will be delivered in 2 days. Deliveries from 10H00 to 18H00, daily. NO DELIEVERIES ON SUNDAYS AND PUBLIC HOLIDAYS
-              </div>
-            </div>
-        <div
-          className="col-12 sticky-top"
-          sx={{
-            maxHeight: "150px",
-            zIndex: "9997",
-            bg: "rgba(255, 255, 255, 0.95)",
-            boxShadow: "0 4px 6px -6px #c7c7c7"
-          }}
-        >
-          <Header />
-        </div>
-        <Layout className="container" sx={{ paddingTop: "20px" }}>
-          <Layout>
-            <Content>
-              <Component {...pageProps} />
-              <CartDrawer />
-            </Content>
-          </Layout>
-          <Footer />
-        </Layout>
-      </ThemeProvider>
-    </CartContextProvider>
-    </ProductContextProvider>
-    </BannerContextProvider>
-  )
+        return () => {
+            Router.events.off("routeChangeStart", handleRouteChange);
+            Router.events.off("routeChangeComplete", handleCompleteChange);
+            Router.events.off("routeChangeError", handleChangeError);
+        };
+    }, []);
+    return (
+        <BannerContextProvider>
+            <ProductContextProvider>
+                <CartContextProvider>
+                    <ThemeProvider theme={theme}>
+                        <div
+                            className="col-12"
+                            sx={{
+                                bg: "muted",
+                                height: "60px",
+                                display: "grid",
+                                color: "#FFFFFF",
+                            }}
+                        >
+                            <div
+                                className="container"
+                                sx={{
+                                    height: "60px",
+                                    display: "grid",
+                                    placeItems: "center",
+                                    color: "#FFFFFF",
+                                }}
+                            >
+                                <p className="mb-0">
+                                    All orders received before 14:00, will be
+                                    delivered in 2 days. Deliveries from 10H00
+                                    to 18H00, daily. NO DELIEVERIES ON SUNDAYS
+                                    AND PUBLIC HOLIDAYS
+                                </p>
+                            </div>
+                        </div>
+                        <div
+                            className="col-12 sticky-top"
+                            sx={{
+                                maxHeight: "150px",
+                                zIndex: "9997",
+                                bg: "rgba(255, 255, 255, 0.95)",
+                                boxShadow: "0 4px 6px -6px #c7c7c7",
+                            }}
+                        >
+                            <Header />
+                        </div>
+                        <Layout
+                            className="container"
+                            sx={{ paddingTop: "50px" }}
+                        >
+                            <Layout>
+                                <Content>
+                                    <Component {...pageProps} />
+                                    <CartDrawer />
+                                </Content>
+                            </Layout>
+                            <Footer />
+                        </Layout>
+                    </ThemeProvider>
+                </CartContextProvider>
+            </ProductContextProvider>
+        </BannerContextProvider>
+    );
 }
 
-export default App
+export default App;
