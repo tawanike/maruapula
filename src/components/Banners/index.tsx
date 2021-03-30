69ffcf8d7172114d952df3d07bd5e8b616c3b286
Tawanda Abraham Makunike <tawanda@mmogomedia.com>
2021-03-30 10:56:31 +0200

    Updated banners

--- src/components/Banners/index.tsx
+++ src/components/Banners/index.tsx
@@ -1,6 +1,27 @@
 import { Button, Carousel } from 'antd';
+import { useRouter } from 'next/router';
+import { useContext } from "react";
+import { ProductContext } from "src/components/Products/context";
+import {
+  filterProducts,
+  selectedCategory,
+} from "src/components/Products/actions";
 
 export default function Banners(props) {
+  const router = useRouter();
+  const productContext = useContext(ProductContext);
+
+  const goTo = (location) => {
+    const defaultCategoryProducts: any[] = productContext.state.products.filter(
+      (product) => {
+          if (product.category === location) {
+              return product;
+          }
+      }
+  );
+  productContext.dispatch(selectedCategory(location));
+  productContext.dispatch(filterProducts(defaultCategoryProducts));
+  }
 
   return <Carousel autoplay>
       {props.banners.map((banner, index) => (<div className="Banner" key={index}>
@@ -8,7 +29,7 @@ export default function Banners(props) {
         <div className="Banner__text">
           <span>{banner.category}</span>
           <h3>{banner.title}</h3>
-          <Button type="primary">Buy Now</Button>
+          <Button type="primary" onClick={() => goTo(banner.link)}>Buy Now</Button>
         </div>
       </div>))}
   </Carousel>
