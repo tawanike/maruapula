6295fdc79905e88ac895a270b2388bb4e0419488
Tatenda <tjmakunike@gmail.com>
2021-04-15 10:04:41 +0200

    added sliding products

--- src/components/Products/List.tsx
+++ src/components/Products/List.tsx
@@ -4,79 +4,110 @@ import { jsx } from "theme-ui";
 import { useState, useEffect } from "react";
 import { Pagination } from "antd";
 import ListItem from "src/components/Products/ListItem";
-
+import { motion } from "framer-motion";
 
 export default function List(props) {
-  const [pageStartEnd, setPageStartEnd] = useState<any>({start: 0, end:12});
-
-  const handleChangePage = (page, pageSize) => {
-    console.log(page, pageSize);
-    console.log(pageStartEnd)
-    let start = 0;
-    let end = pageSize;
-    if (page > 1) {
-      start = pageStartEnd.end;
-      end = (pageSize) * page;
-    }
-
-    if (page === 2) {
-      start = 13;
-      end = 24;
-    }
-
-    if (page > 3) {
-      start = 25
-      end = 36;
-    }
-
-    if (page > 4) {
-      start = 37;
-      end = 48;
-    }
-
-    if (page > 5) {
-      start = 49;
-      end = 60;
-    }
-
-    props.changePage(page);
-    setPageStartEnd({start, end});
-    window.scrollTo({
-      top: 550,
-      left: 0,
-      behavior: 'smooth'
+    const [pageStartEnd, setPageStartEnd] = useState<any>({
+        start: 0,
+        end: 12,
     });
-  }
 
-  useEffect(() => {
-    if (props.currentPage === 1) {
-      setPageStartEnd({start: 0, end: 12});
-    }
-  }, [props.currentPage]);
+    let easing = [0.175, 0.85, 0.42, 0.96];
+    const imageVariants = {
+        exit: {
+            y: -80,
+            opacity: 0,
+            transition: { duration: 0.2, ease: easing },
+        },
+        enter: {
+            y: 0,
+            opacity: 1,
+            transition: {
+                duration: 0.5,
+                ease: easing,
+            },
+        },
+    };
 
-  return (
-    <div className="col-12">
-      <h1 className="pb-4" sx={{ fontSize: "38px" }}>
-        { 
-          props.category.length !== 0 ?
-          props.category === 'Precooked' ? 'Pre-cooked Meals' : 
-          props.category === 'Specials' ? 'Today\'s Specials' : 
-          'Fresh '+ props.category 
-          : ''
+    const handleChangePage = (page, pageSize) => {
+        console.log(page, pageSize);
+        console.log(pageStartEnd);
+        let start = 0;
+        let end = pageSize;
+        if (page > 1) {
+            start = pageStartEnd.end;
+            end = pageSize * page;
         }
-      </h1>
-      <div className="row" sx={{}}>
-        {props.products && props.products.slice(pageStartEnd.start, pageStartEnd.end).map(product => <ListItem key={product.id} product={product} />)}
-      </div>
-      <div className="row">
-        <Pagination 
-          current={props.currentPage} 
-          total={props.products && props.products.length} 
-          pageSize={12}
-          hideOnSinglePage={true}
-          onChange={handleChangePage} 
-        />
-      </div>
-    </div>
-  )
+
+        if (page === 2) {
+            start = 13;
+            end = 24;
+        }
+
+        if (page > 3) {
+            start = 25;
+            end = 36;
+        }
+
+        if (page > 4) {
+            start = 37;
+            end = 48;
+        }
+
+        if (page > 5) {
+            start = 49;
+            end = 60;
+        }
+
+        props.changePage(page);
+        setPageStartEnd({ start, end });
+        window.scrollTo({
+            top: 550,
+            left: 0,
+            behavior: "smooth",
+        });
+    };
+
+    useEffect(() => {
+        if (props.currentPage === 1) {
+            setPageStartEnd({ start: 0, end: 12 });
+        }
+    }, [props.currentPage]);
+
+    return (
+        // <div className="col-12">
+        <motion.div
+            className="row"
+            variants={imageVariants}
+            sx={{ overflow: "hidden" }}
+        >
+            <h1 className="pb-4" sx={{ fontSize: "38px" }}>
+                {props.category.length !== 0
+                    ? props.category === "Precooked"
+                        ? "Pre-cooked Meals"
+                        : props.category === "Specials"
+                        ? "Today's Specials"
+                        : "Fresh " + props.category
+                    : ""}
+            </h1>
+            <div className="row">
+                {props.products &&
+                    props.products
+                        .slice(pageStartEnd.start, pageStartEnd.end)
+                        .map((product) => (
+                            <ListItem key={product.id} product={product} />
+                        ))}
+            </div>
+            <div className="row">
+                <Pagination
+                    current={props.currentPage}
+                    total={props.products && props.products.length}
+                    pageSize={12}
+                    hideOnSinglePage={true}
+                    onChange={handleChangePage}
+                />
+            </div>
+        </motion.div>
+        // {/* </div> */}
+    );
 }
--- src/components/Products/ListItem.tsx
+++ src/components/Products/ListItem.tsx
@@ -1,122 +1,199 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { jsx } from "theme-ui"
+import { jsx } from "theme-ui";
 
-import { Modal } from "antd"
-import { useState } from "react"
-import Image from "next/image"
+import { Modal } from "antd";
+import { useState } from "react";
+import Image from "next/image";
 
-import AddToCartButton from "src/components/Cart/Button"
+import AddToCartButton from "src/components/Cart/Button";
+import { motion } from "framer-motion";
 
 export default function ListItem(props) {
-  const [showProduct, setShowProduct] = useState(false)
-  const productDetails = () => {
-    setShowProduct(true)
-  }
+    const [showProduct, setShowProduct] = useState(false);
+    const productDetails = () => {
+        setShowProduct(true);
+    };
 
-  const getProductImg = id => {
-    id ? id : "nothing"
+    let easing = [0.175, 0.85, 0.42, 0.96];
+    const imageVariants = {
+        exit: {
+            x: -80,
+            opacity: 0,
+            transition: { duration: 0.2, ease: easing },
+        },
+        enter: {
+            x: 0,
+            opacity: 1,
+            transition: {
+                duration: 0.5,
+                ease: easing,
+            },
+        },
+    };
 
-    if (id) {
-      return `https://res.cloudinary.com/maruapula/image/upload/v1618326830/products/${id}.jpg`
-    }
+    const getProductImg = (id) => {
+        id ? id : "nothing";
 
-    return `./placeholder.jpg`
-  }
+        if (id) {
+            return `https://res.cloudinary.com/maruapula/image/upload/v1618326830/products/${id}.jpg`;
+        }
 
-  return (
-    <div className="col-12 col-md-4" sx={{ marginBottom: "25px" }}>
-      <div
-        className=""
-        sx={{
-          variant: "containers.card",
-          height: "100%",
-          "&:hover": { cursor: "pointer" },
-          display: "grid"
-        }}
-      >
-        <div className="col-12" sx={{ variant: "center" }}>
-          <a sx={{ variant: "center" }} href={`#`} onClick={productDetails}>
-            <img
-              className="Product__Image"
-              src={getProductImg(props.product.id)}
-              alt={props.product.title}
-              onError={e => {
-                ;(e.target as any).src = "./placeholder.png"
-                ;(e.target as any).style.width = "40%"
-                ;(e.target as any).style.height = "auto"
-                ;(e.target as any).style.marginTop = "15px"
-                ;(e.target as any).style.marginBottom = "15px"
-              }}
-              sx={{ width: "80%", height: "auto" }}
-            />
-          </a>
-        </div>
-        <div className="col-12" sx={{ variant: "center" }}>
-          <p sx={{ variant: "text.subHead" }}>{props.product.title}</p>
-        </div>
-        <div className="col-12" sx={{ variant: "center" }}>
-          <p sx={{ whiteSpace: "pre-wrap", py: "5px", fontSize: "18px" }}>{props.product.size}</p>
-        </div>
-        <div className="col-12" sx={{ variant: "center" }}>
-          <p sx={{ fontSize: "20px", fontWeight: "700", color: "muted" }}>
-            {props.product.promo_price > 0 ? (
-              <>
-                <span className="price">R{props.product.promo_price}</span>&nbsp;
-                <span className="promo-price">R{props.product.price}</span>
-              </>
-            ) : (
-              <span className="price">R{props.product.price}</span>
-            )}
-          </p>
-        </div>
+        return `./placeholder.jpg`;
+    };
 
-        <AddToCartButton product={props.product} />
-        <Modal title={``} 
-          onCancel={() => setShowProduct(false)} 
-          visible={showProduct} 
-          footer={null}
-          style={{ top: 160 }}
-          width={`60%`}
-          >
-          <div className="row">
-            <div className="col-5">
-            <img
-              className="Product__Image"
-              src={getProductImg(props.product.id)}
-              alt={props.product.title}
-              onError={e => {
-                ;(e.target as any).src = "./placeholder.png"
-                ;(e.target as any).style.width = "40%"
-                ;(e.target as any).style.height = "auto"
-                ;(e.target as any).style.marginTop = "15px"
-                ;(e.target as any).style.marginBottom = "15px"
-              }}
-              sx={{ width: "80%", height: "auto" }}
-            />
-            </div>
-            <div className="col-7">
-              <h1>{props.product.title}</h1>
-              <p sx={{ fontSize: "20px", fontWeight: "700", color: "muted" }}>
-                {props.product.promo_price > 0 ? (
-                  <>
-                    <span className="price">R{props.product.promo_price}</span>&nbsp;
-                    <span className="promo-price">R{props.product.price}</span>
-                  </>
-                ) : (
-                  <span className="price">R{props.product.price}</span>
-                )}
-              </p>
-              <p sx={{ whiteSpace: "pre-wrap", py: "5px", fontSize: "18px" }}>{props.product.size}</p>
-            
-              <AddToCartButton product={props.product} />
+    return (
+        //   <div className="col-12 col-md-4" sx={{ marginBottom: "25px" }}>
+        <motion.div
+            className="col-12 col-md-4"
+            initial="exit"
+            animate="enter"
+            exit="exit"
+            sx={{ marginBottom: "25px" }}
+        >
+            <motion.div className="h-100" variants={imageVariants}>
+                <div
+                    sx={{
+                        variant: "containers.card",
+                        height: "100%",
+                        "&:hover": { cursor: "pointer" },
+                        display: "grid",
+                    }}
+                >
+                    <div className="col-12" sx={{ variant: "center" }}>
+                        <a
+                            sx={{ variant: "center" }}
+                            href={`#`}
+                            onClick={productDetails}
+                        >
+                            <img
+                                className="Product__Image"
+                                src={getProductImg(props.product.id)}
+                                alt={props.product.title}
+                                onError={(e) => {
+                                    (e.target as any).src = "./placeholder.png";
+                                    (e.target as any).style.width = "40%";
+                                    (e.target as any).style.height = "auto";
+                                    (e.target as any).style.marginTop = "15px";
+                                    (e.target as any).style.marginBottom =
+                                        "15px";
+                                }}
+                                sx={{ width: "80%", height: "auto" }}
+                            />
+                        </a>
+                    </div>
+                    <div className="col-12" sx={{ variant: "center" }}>
+                        <p sx={{ variant: "text.subHead" }}>
+                            {props.product.title}
+                        </p>
+                    </div>
+                    <div className="col-12" sx={{ variant: "center" }}>
+                        <p
+                            sx={{
+                                whiteSpace: "pre-wrap",
+                                py: "5px",
+                                fontSize: "18px",
+                            }}
+                        >
+                            {props.product.size}
+                        </p>
+                    </div>
+                    <div className="col-12" sx={{ variant: "center" }}>
+                        <p
+                            sx={{
+                                fontSize: "20px",
+                                fontWeight: "700",
+                                color: "muted",
+                            }}
+                        >
+                            {props.product.promo_price > 0 ? (
+                                <>
+                                    <span className="price">
+                                        R{props.product.promo_price}
+                                    </span>
+                                    &nbsp;
+                                    <span className="promo-price">
+                                        R{props.product.price}
+                                    </span>
+                                </>
+                            ) : (
+                                <span className="price">
+                                    R{props.product.price}
+                                </span>
+                            )}
+                        </p>
+                    </div>
 
-            </div>
-          </div>
-          
-          
-        </Modal>
-      </div>
-    </div>
-  )
+                    <AddToCartButton product={props.product} />
+                    <Modal
+                        title={``}
+                        onCancel={() => setShowProduct(false)}
+                        visible={showProduct}
+                        footer={null}
+                        style={{ top: 160 }}
+                        width={`60%`}
+                    >
+                        <div className="row">
+                            <div className="col-5">
+                                <img
+                                    className="Product__Image"
+                                    src={getProductImg(props.product.id)}
+                                    alt={props.product.title}
+                                    onError={(e) => {
+                                        (e.target as any).src =
+                                            "./placeholder.png";
+                                        (e.target as any).style.width = "40%";
+                                        (e.target as any).style.height = "auto";
+                                        (e.target as any).style.marginTop =
+                                            "15px";
+                                        (e.target as any).style.marginBottom =
+                                            "15px";
+                                    }}
+                                    sx={{ width: "80%", height: "auto" }}
+                                />
+                            </div>
+                            <div className="col-7">
+                                <h1>{props.product.title}</h1>
+                                <p
+                                    sx={{
+                                        fontSize: "20px",
+                                        fontWeight: "700",
+                                        color: "muted",
+                                    }}
+                                >
+                                    {props.product.promo_price > 0 ? (
+                                        <>
+                                            <span className="price">
+                                                R{props.product.promo_price}
+                                            </span>
+                                            &nbsp;
+                                            <span className="promo-price">
+                                                R{props.product.price}
+                                            </span>
+                                        </>
+                                    ) : (
+                                        <span className="price">
+                                            R{props.product.price}
+                                        </span>
+                                    )}
+                                </p>
+                                <p
+                                    sx={{
+                                        whiteSpace: "pre-wrap",
+                                        py: "5px",
+                                        fontSize: "18px",
+                                    }}
+                                >
+                                    {props.product.size}
+                                </p>
+
+                                <AddToCartButton product={props.product} />
+                            </div>
+                        </div>
+                    </Modal>
+                </div>
+            </motion.div>
+        </motion.div>
+        //   </div>
+    );
 }
