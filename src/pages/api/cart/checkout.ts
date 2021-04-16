c8f6df63b6d91ff901de880d5e1a37a4e0b7c27b
Tawanda Abraham Makunike <tawanda@mmogomedia.com>
2021-04-16 15:42:03 +0200

    Changesa

--- src/pages/api/cart/checkout.ts
+++ src/pages/api/cart/checkout.ts
@@ -16,7 +16,7 @@ export default async (req, res) => {
             dynamic_template_data: {
                 subject: 'Online Order Confirmation',
                 service_fee: 'R50.00',
-                total: parseInt(req.body.total) + parseInt(50),
+                total: parseInt(req.body.total) + parseInt('50'),
                 tracking_code: req.body.order_reference,
                 customer_name: req.body.user.first_name,
                 customer_email: req.body.user.email,
@@ -49,7 +49,7 @@ export default async (req, res) => {
             dynamic_template_data: {
               subject: 'Online Order Confirmation',
               service_fee: 'R50.00',
-              total: parseInt(req.body.total) + parseInt(50),
+              total: parseInt(req.body.total) + parseInt('50'),
               tracking_code: req.body.order_reference,
               customer_name: req.body.user.first_name,
               customer_email: req.body.user.email,
